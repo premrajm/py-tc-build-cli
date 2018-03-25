0db3601e12e898269887cafe24771a198df1971e
Premraj Motling <premraj.motling@gmail.com>
2018-03-25 17:24:45 +0530

    Refactor to use options so command can be executed without prompts

--- tc/cli.py
+++ tc/cli.py
@@ -18,11 +18,11 @@ def status():
 
 
 @main.command()
-def login():
+@click.option('--server', prompt='Team city Server', help='team city server host name without port (e.g. localhost)')
+@click.option('--username', prompt='Username', help='team city username')
+@click.option('--password', prompt='Password', help='team city password', hide_input=True)
+def login(server, username, password):
     """configure login credentials for tc server"""
-    server = click.prompt('Please enter server host name [without port e.g localhost]', type=click.STRING)
-    username = click.prompt('Please enter username', type=click.STRING)
-    password = click.prompt('Please enter password', type=click.STRING, hide_input=True)
     auth = '{0}:{1}'.format(username, password)
     utils.writeFile(authConfig.format(server), base64.b64encode(auth.encode()).decode('utf-8'))
 
