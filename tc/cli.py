f27a69044f545c3860e1682cdba20db2f8e76a81
Premraj Motling <premraj.motling@gmail.com>
2018-03-25 16:17:45 +0530

    Add function to save credentials

--- tc/cli.py
+++ tc/cli.py
@@ -1,10 +1,30 @@
+import base64
 import click
+import os
+import utils
+
+authConfig = os.environ.get('HOME') + "/.tcbuild/{0}.auth"
+
 
 @click.group()
 def main():
     pass
 
+
 @main.command()
 def status():
     """Get the build related info from team city server"""
     click.echo('SUCCESS')
+
+
+@main.command()
+def login():
+    """configure login credentials for tc server"""
+    server = click.prompt('Please enter server host name [without port e.g localhost]', type=click.STRING)
+    username = click.prompt('Please enter username', type=click.STRING)
+    password = click.prompt('Please enter password', type=click.STRING, hide_input=True)
+    auth = '{0}:{1}'.format(username, password)
+    file = utils.createAndOpen(authConfig.format(server), 'w')
+    file.write(base64.b64encode(auth.encode()).decode('utf-8'))
+    file.close()
+
---
+++ tc/utils.py
@@ -0,0 +1,4 @@
+
+def createAndOpen(filename, mode):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    return open(filename, mode)
