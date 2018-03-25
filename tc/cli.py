12f372672feddb59f6b51a4967eb8f0e3c9c42f9
Premraj Motling <premraj.motling@gmail.com>
2018-03-25 23:53:29 +0530

    Move config types to configtypes.py

--- tc/cli.py
+++ tc/cli.py
@@ -2,22 +2,8 @@ import click
 import configparser
 import os
 
+from tc.configtypes import *
 from tc.error import *
-
-
-class BuildConfig:
-    config_file = 'tc-build.ini'
-    main = 'Main'
-    main_server = 'server'
-    main_type_id = 'type_id'
-
-
-class AuthConfig:
-    config_file = os.environ.get('HOME') + '/.tcbuild/{0}.auth'
-    auth = 'Auth'
-    auth_user = 'user'
-    auth_pass = 'password'
-
 
 build_config = BuildConfig()
 auth_config = AuthConfig()
---
+++ tc/configtypes.py
@@ -0,0 +1,12 @@
+class BuildConfig:
+    config_file = 'tc-build.ini'
+    main = 'Main'
+    main_server = 'server'
+    main_type_id = 'type_id'
+
+
+class AuthConfig:
+    config_file = os.environ.get('HOME') + '/.tcbuild/{0}.auth'
+    auth = 'Auth'
+    auth_user = 'user'
+    auth_pass = 'password'
